57ea82b618a69c62d4adcea832cc17f823959ac7
Martyn Loughran <me@mloughran.com>
2009-06-24 13:07:29 +0100

    Test adapter: fixed interface and added functionality:
    * Now supports named queue
    * Now supports queue inspection

--- lib/job_queue/adapters/test_adapter.rb
+++ lib/job_queue/adapters/test_adapter.rb
@@ -1,20 +1,49 @@
+# This adapter is designed for testing purposes.
+#
+# Features supported:
+#
+# named queues: yes
+# priority: no
+# ttr: no
+#
+# Additionally this queue can be inspeced with JobQueue.adapter.queue('name')
+#
 class JobQueue::TestAdapter
   def initialize(options = {})
-    @queue = []
+    @queues = {}
   end
-  
-  def put(string)
-    @queue << string
+
+  def put(string, queue, priority, ttr)
+    @queues[queue] ||= []
+    @queues[queue] << string
   end
-  
-  def subscribe(error_report, &block)
+
+  def subscribe(error_report, cleanup_task, queue, &block)
     loop do
       begin
-        sleep 0.1 if @queue.empty?
-        yield @queue.shift
-      rescue
-        error_report.call(job.body, e)
+        if get_queue(queue).empty?
+          sleep 0.1
+        else
+          job = get_queue(queue).shift
+          yield job
+        end
+      rescue => e
+        error_report.call(job, e)
       end
     end
+  end
+
+  # Additional method for TestAdapter to allow easy queue inspection with
+  #
+  # JobQueue.adapter.queue('foo')
+  #
+  def queue(queue = 'default')
+    get_queue(queue)
+  end
+
+  private
+
+  def get_queue(queue)
+    @queues[queue] || []
   end
 end
--- spec/test_adapter_spec.rb
+++ spec/test_adapter_spec.rb
@@ -1,39 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/common_adapter_spec'
 
 describe JobQueue::TestAdapter do
   before :all do
     JobQueue.adapter = JobQueue::TestAdapter.new
   end
   
-  it "should write onto queue and fetch stuff back off" do
-    JobQueue.put("hello")
-    
-    JobQueue.subscribe do |job|
-      @job = job
-      throw :stop
-    end
-    
-    @job.should == "hello"
-  end
+  it_should_behave_like 'JobQueue adapter basic'
   
-  it "should pull items off in the order the were added" do
-    JobQueue.put("foo")
-    JobQueue.put("bar")
-    
-    retrieved_jobs = []
-    
-    begin
-      Timeout::timeout(0.5) do
-        JobQueue.subscribe do |job|
-          retrieved_jobs << job
-        end
-      end
-    rescue Timeout::Error
-      
-    end
-    
-    retrieved_jobs[0].should == "foo"
-    retrieved_jobs[1].should == "bar"
-    retrieved_jobs[2].should == nil
+  it_should_behave_like "JobQueue adapter named queues"
+  
+  it "should allow queue inspection as a hash" do
+    JobQueue.adapter.queue.should == []
+    JobQueue.put('hello')
+    JobQueue.adapter.queue.should == ['hello']
   end
 end
